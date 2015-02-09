70506e0a282ab0e964eb89718833b56f2d62a2e6
Ian McCunn <ianmccunn@gmail.com>
2015-02-09 11:48:21 -0800

    putting init test params in before statement

--- test/bmpObjectTests.js
+++ test/bmpObjectTests.js
@@ -10,30 +10,30 @@ var fs = require('fs');
 
 describe('bmp module contructor should read a bitmap buffer and set its header properties', function() {
   
-  var bmpBuf = fs.readFileSync('./test/test.bmp');
+  before(function(){
+    this.bmpBuf = fs.readFileSync('./test/test.bmp');
+    this.testBmpObj = new bmp.Bitmap(this.bmpBuf);
+    this.testBmpObj2 = new bmp.Bitmap(this.bmpBuf);
+    this.initialPalette = this.testBmpObj.palette;
+    this.modifiedPalette = this.testBmpObj2.palette;
 
-  var testBmpObj = new bmp.Bitmap(bmpBuf);
-  var testBmpObj2 = new bmp.Bitmap(bmpBuf);
-
-  var initialPalette = testBmpObj.palette,
-      modifiedPalette = testBmpObj2.palette;
-
-  testBmpObj2.transformPalette();
+    this.testBmpObj2.transformPalette();
+  });
 
   it('should have a passed parameter of type Buffer', function() {
-    expect(Buffer.isBuffer(bmpBuf)).to.eql(true); 
+    expect(Buffer.isBuffer(this.bmpBuf)).to.eql(true); 
   });
 
   it('should have a type of BM', function() {
-    expect(testBmpObj.type).to.eql('BM');
+    expect(this.testBmpObj.type).to.eql('BM');
   });
 
   it('should have a palette offset of 54', function() {
-    expect(testBmpObj.paletteOffset).to.eql(54);
+    expect(this.testBmpObj.paletteOffset).to.eql(54);
   });
 
   it('should modify the palette information', function() {
-    expect(initialPalette).to.not.eql(modifiedPalette);
+    expect(this.initialPalette).to.not.eql(this.modifiedPalette);
   });
 
 });
